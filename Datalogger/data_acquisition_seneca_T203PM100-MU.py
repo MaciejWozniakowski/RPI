0db100e5031ba8234e1941ac3d41edd677c24bb7
MaciejWozniakowski <maciejwozniakowski02@gmail.com>
2024-10-06 21:23:50 +0200

    data acqisition compiles

---
+++ Datalogger/data_acquisition_seneca_T203PM100-MU.py
@@ -0,0 +1,39 @@
+import pymodbus.client as ModbusClient  
+from pymodbus import (
+    ExceptionResponse,
+    ModbusException,
+    pymodbus_apply_logging_config,
+) 
+import time 
+import asyncio
+
+baudrate = 9600 #wybrany z palca
+port = 5020
+
+        
+async def run_async_simple_client(port ):
+    client = ModbusClient.AsyncModbusSerialClient(
+        port,
+        baudrate = 9600,
+        bytesize = 8,
+        parity = "N",
+        stopbits = 1
+    )
+
+    #start the client
+    print("connect to server")
+    await client.connect()
+    #test if the client has been connnected 
+    assert client.connected
+    print("connection established, reading data")
+
+
+    try:
+        rr = await client.read_holding_registers()
+    except ModbusException as exc:
+        print(f"Recieved exception {exc}")
+        client.close()
+        return
+
+
+
