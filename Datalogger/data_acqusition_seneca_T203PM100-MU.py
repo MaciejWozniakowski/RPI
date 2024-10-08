02abd408eb354ebb15c50f6e123394a445033881
Maciej Woźniakowski <96246938+MaciejWozniakowski@users.noreply.github.com>
2024-10-08 21:44:47 +0200

    Update data_acqusition_seneca_T203PM100-MU.py

--- Datalogger/data_acqusition_seneca_T203PM100-MU.py
+++ Datalogger/data_acqusition_seneca_T203PM100-MU.py
@@ -1,40 +1,36 @@
-import time
-from pymodbus.client.sync import ModbusSerialClient as ModbusClient
+import pymodbus.client as ModbusClient  
+from pymodbus import (
+    ExceptionResponse,
+    ModbusException,
+    pymodbus_apply_logging_config,
+) 
+import time 
+import asyncio
 
-client = ModbusClient(
-    method='rtu',
-    port='',  #do zadecydowania z faktycznym miernikiem i raspberry 
-    baudrate=9600,        
-    parity='N',           
-    stopbits=1,
-    bytesize=8,
-    timeout=1
-)
+baudrate = 9600 #wybrany z palca
+port = 5020
 
-client.connect()
+        
+async def run_async_simple_client(port ):
+    client = ModbusClient.AsyncModbusSerialClient(
+        port,
+        baudrate = 9600,
+        bytesize = 8,
+        parity = "N",
+        stopbits = 1
+    )
 
-def read_voltage():
+    #start the client
+    print("connect to server")
+    await client.connect()
+    #test if the client has been connnected 
+    assert client.connected
+    print("connection established, reading data")
+
+
     try:
-        response = client.read_holding_registers(40099, 1, unit=1) #czytamy z miernika nr 1 stad unit=1 
-
-        if not response.isError():
-            voltage = response.registers[0]
-
-
-            print(f"Voltage: {voltage} V")
-        else:
-            print("Failed to read from register")
-    except Exception as e:
-        print(f"Error: {e}")
-
-# Main loop to read voltage periodically
-try:
-    while True:
-        read_voltage()
-        time.sleep(1)  # Read every 5 seconds (adjust as needed)
-except KeyboardInterrupt:
-    print("Exiting program...")
-finally:
-    # Close the Modbus client connection
-    client.close()
-
+        await client.read_holding_registers()
+    except ModbusException as exc:
+        print(f"Recieved exception {exc}")
+        client.close()
+        return
